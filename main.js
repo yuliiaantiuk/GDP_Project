6088f549cf87ed982ce3c7a8459f5e4decce624a
Yulia Antiuk <uliaantiuk@gmail.com>
2025-03-30 11:38:15 +0300

    added university and filter page

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-// адаптивне меню
+
 document.addEventListener("DOMContentLoaded", function () {
+    // адаптивне меню
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
 
     hamburger.addEventListener("click", function () {
         navMenu.classList.toggle("active");
     });
+
+    // поява фільтрів
+    const filterBtn = document.querySelector(".filter-btn");
+    const filterContainer = document.querySelector(".filter-container");
+
+    filterBtn.addEventListener("click", function () {
+        filterContainer.classList.toggle("show-filters");
+    });
+
+    // закриття фільтрів
+    const cancelBtn = document.querySelector(".cancel-btn");
+    cancelBtn.addEventListener("click", function () {
+        filterContainer.classList.toggle("show-filters");
+    })
 });
 
 // акордеон faq
@@ -22,3 +37,6 @@ document.querySelectorAll('.accordion-header').forEach(button => {
         content.style.display = isActive ? 'none' : 'block';
     });
 });
+
+
+
