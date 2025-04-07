7a6ca58c932d7afa5d163fde3b19f4e9d127b121
Yulia Antiuk <uliaantiuk@gmail.com>
2025-04-07 22:00:52 +0300

    Added faculty and unversity page

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
 
-    hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
+    if(hamburger){
+        hamburger.addEventListener("click", function () {
+            navMenu.classList.toggle("active");
+        });
+    }
 
     // поява фільтрів
     const filterBtn = document.querySelector(".filter-btn");
